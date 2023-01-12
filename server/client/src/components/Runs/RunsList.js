fd06063f45ed2db19b9ea04cd12e72b1ba624139
jessikim <jessica.kim17@gmail.com>
2023-01-12 15:27:29 -0600

    remove runs component and set up runslist new component

---
+++ server/client/src/components/Runs/RunsList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RunsList = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Join a Run
+      </h1>
+      Sign up for a run
+    </div>
+  )
+}
+
+export default RunsList
